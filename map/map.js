7ef5a79bea2c8d3a59ab47a864d6583193171376
yzrsamet <yzr.samet@gmail.com>
2020-11-10 01:01:10 -0800

    map funtion used r and b colors to map crcles x and y values

--- map/map.js
+++ map/map.js
@@ -6,7 +6,7 @@ var circle1 = {
   diameter:50
 };
 var circle2 = {
-  x:0,
+  x:600,
   y:200,
   diameter:50
 };
@@ -19,9 +19,10 @@ function setup() {
 
 function draw() {
   //map(mouseX,min1,max1,min2,max2)
-  col = map(mouseX,600,0,0,255);
+  r = map(circle1.x,600,0,0,255);
+  b = map(circle2.y+circle2.x,600,0,255,0);
 
-  background(col);
+  background(r,0,b);
 
 
 
@@ -32,9 +33,12 @@ function draw() {
   ellipse(circle1.x,circle1.y,circle1.diameter);
 
   if(circle1.x !== width ) {
-  circle2.x = circle2.x + 1;
+
   circle1.x = circle1.x + 1;
-}
+  }
+  if(circle2.x > 0) {
+    circle2.x = circle2.x - 1;
+  }
 
   if(circle2.diameter < (3 * circle1.diameter)) {
   circle2.diameter = circle2.diameter + 1;
